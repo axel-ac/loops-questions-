b9d655b74ab9014af3fd9887bee2ef48c16be076
axel-ac <abdullahceylan.axel@gmail.com>
2023-04-14 00:18:07 +0300

    for

---
+++ for.js
@@ -0,0 +1,14 @@
+// * =======================================================
+
+// *                      FOR LOOP
+
+// * =======================================================
+
+// ! Question-1
+// ? Can you make a program that prints hello 10 times?
+
+// ! Answer-1
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`${i}: Hello`);
+}
